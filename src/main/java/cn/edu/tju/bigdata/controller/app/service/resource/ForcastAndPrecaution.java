94943abf663821bed6d0e6e775ed1fa6c2994b0f
itlucas <1473810935@qq.com>
2016-10-09 22:05:40 +0800

    chc-20161009

---
+++ src/main/java/cn/edu/tju/bigdata/controller/app/service/resource/ForcastAndPrecaution.java
@@ -0,0 +1,16 @@
+package cn.edu.tju.bigdata.controller.app.service.resource;
+
+import cn.edu.tju.bigdata.controller.index.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by chc on 2016/10/9.
+ */
+@Controller
+@RequestMapping("/forcast")
+public class ForcastAndPrecaution extends BaseController{
+
+
+
+}
